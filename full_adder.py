2ca7a83ef6165a79b17dde8e6c945d3bfee53099
summonholmes <shanekimble12@hotmail.com>
2018-02-05 03:02:24 -0500

    All code is now more Pythonic

--- full_adder.py
+++ full_adder.py
@@ -3,19 +3,24 @@ from bit_truth import bit_truth
 
 
 def full_adder(key_17, key_25, bin_arr):
-	# Get the lsfr outputs, group by 8 elements, then perform full adder in bit_truth.py.
-	lfsr_17 = lfsr(key_17,  [17, 3], bin_arr)
-	lfsr_25 = lfsr(key_25, [25, 8, 6, 2], bin_arr)
-	split_lfsr_17 = [lfsr_17[start:start + 8] for start in range(0, len(lfsr_17), 8)]
-	split_lfsr_25 = [lfsr_25[start:start + 8] for start in range(0, len(lfsr_25), 8)]
+    # Get the lsfr outputs, group by 8 elements, then perform full adder in
+    # bit_truth.py.
+    lfsr_17 = lfsr(key_17,  [17, 3], bin_arr)
+    lfsr_25 = lfsr(key_25, [25, 8, 6, 2], bin_arr)
+    split_lfsr_17 = [lfsr_17[start:start + 8]
+                     for start in range(0, len(lfsr_17), 8)]
+    split_lfsr_25 = [lfsr_25[start:start + 8]
+                     for start in range(0, len(lfsr_25), 8)]
 
-	# Retrieve the plaintext-lfsr bit xor output from the truth table.
-	crypt_bin = bit_truth(bin_arr, split_lfsr_17, split_lfsr_25)
+    # Retrieve the plaintext-lfsr bit xor output from the truth table.
+    crypt_bin = bit_truth(bin_arr, split_lfsr_17, split_lfsr_25)
 
-	# Convert the binary data to strings of 8 characters (So the process can repeat itself).
-	for i in range(len(crypt_bin)):
-		crypt_bin[i] = str(crypt_bin[i])
-	crypt_bin_str = "".join(crypt_bin)
-	crypt_bin_arr = [crypt_bin_str[start:start+8] for start in range(0, len(crypt_bin_str), 8)]
+    # Convert the binary data to strings of 8 characters (So the process can
+    # repeat itself).
+    for i in range(len(crypt_bin)):
+        crypt_bin[i] = str(crypt_bin[i])
+    crypt_bin_str = "".join(crypt_bin)
+    crypt_bin_arr = [crypt_bin_str[start:start + 8]
+                     for start in range(0, len(crypt_bin_str), 8)]
 
-	return crypt_bin_arr
+    return crypt_bin_arr
--- main.py
+++ main.py
@@ -5,39 +5,32 @@ import convert
 
 
 def main():
-	# Initialize the keyword and plaintext as strings.
-	keyword = "Shane"
-	key_arr = list(keyword)
-	key_bin_arr = []
-	# plaintext = "ThisProjectMakesMeWantToPullMyHairOut"
-	plaintext = "OnemansmagicisanothermansengineeringSupernaturalisanullwordOnemanstheologyisanothermansbellylaughProgressisntmadebyearlyrisersItsmadebylazymentryingtofindeasierwaystodosomething"
-	plaintext_arr = list(plaintext)
-	plaintext_bin_arr = []
+    # Initialize the keyword and plaintext as strings.
+    # Perform string to binary conversion for the keyword and plaintext.
+    keyword = "Shane"
+    key_bin_arr = [(bin(ord(list(keyword)[i]))).replace("b", "")
+                   for i in range(len(list(keyword)))]
+    plaintext = (
+        'OnemansmagicisanothermansengineeringSupernaturalisanullword'
+        'OnemanstheologyisanothermansbellylaughProgressisntmadebyearly'
+        'risersItsmadebylazymentryingtofindeasierwaystodosomething')
+    plaintext_bin_arr = [(bin(ord(list(plaintext)[i]))).replace("b", "")
+                         for i in range(len(list(plaintext)))]
 
-	# Perform string to binary conversion for the keyword and plaintext.
-	for i in range(len(key_arr)):
-		key_bin_arr.append(bin(ord(key_arr[i])))
-		key_bin_arr[i] = key_bin_arr[i].replace("b", "")
-	for j in range(len(plaintext_arr)):
-		plaintext_bin_arr.append(bin(ord(plaintext_arr[j])))
-		plaintext_bin_arr[j] = plaintext_bin_arr[j].replace("b", "")
+    # Perform the full encryption/decryption routine.
+    key_17 = insert_1(key_bin_arr[0] + key_bin_arr[1])
+    key_25 = insert_1(key_bin_arr[2] + key_bin_arr[3] + key_bin_arr[4])
+    ciphertext_bin_arr = full_adder(key_17, key_25, plaintext_bin_arr)
+    decrypt_text_bin_arr = full_adder(key_17, key_25, ciphertext_bin_arr)
 
-	# Perform the full encryption/decryption routine.
-	key_17 = insert_1(key_bin_arr[0] + key_bin_arr[1])
-	key_25 = insert_1(key_bin_arr[2] + key_bin_arr[3] + key_bin_arr[4])
-	ciphertext_bin_arr = full_adder(key_17, key_25, plaintext_bin_arr)
-	decrypt_text_bin_arr = full_adder(key_17, key_25, ciphertext_bin_arr)
-
-	# Print the final results.  Output is formatted to demonstrate that the binary conversion process works.
-	print("\nThe keyword: ", keyword)
-	print("The plaintext: ", convert.convert_key_bin_arr(plaintext_bin_arr))
-	print("The ciphertext: ", convert.convert_enc_key(ciphertext_bin_arr))
-	print("The decrypted ciphertext: ", convert.convert_dec_key(decrypt_text_bin_arr))
-	print("Plaintext binary array: ", plaintext_bin_arr)
-	print("Ciphertext binary array: ", ciphertext_bin_arr)
-	print("Decrypted ciphertext binary array: ", decrypt_text_bin_arr)
-	input("\nPress enter to exit")
-
-	return 0
+    # Print the final results.  Output is formatted to demonstrate that the
+    # binary conversion process works.
+    print("\nThe keyword: ", keyword)
+    print("The plaintext: ",
+          convert.convert_key_bin_arr(plaintext_bin_arr)[0:75])
+    print("The ciphertext: ",
+          convert.convert_enc_key(ciphertext_bin_arr)[0:75])
+    print("The decrypted ciphertext: ",
+          convert.convert_dec_key(decrypt_text_bin_arr)[0:75])
 
 main()
