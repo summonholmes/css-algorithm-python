1f86eb1a1fd8089f93d890147a0043a81acd16c4
Shane Kimble <shanekimble@Shanes-MacBook-Pro.local>
2018-07-09 15:44:09 -0400

    Improve

--- convert_bin.py
+++
@@ -1,6 +0,0 @@
-def convert_bin(bin_arr):
-    # Binary array to string
-    str_text = ''.join(str(i) for i in bin_arr)
-    text = ''.join(
-        chr(int(str_text[i:i + 8], 2)) for i in range(0, len(str_text), 8))
-    return text
